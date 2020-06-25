0fea35cb7a5d3adb83a5612b2a30a2058ca0f771
naoya-sumi0613 <s.naoya613pcstudy@gmail.com>
2020-06-25 09:20:15 +0900

    Add rspec users/requests edit

--- spec/requests/users/users_spec.rb
+++ spec/requests/users/users_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 RSpec.describe "Usersコントローラーのテスト", type: :request do
   gimei = Gimei.name
   let(:user) { create(:user) }
-  let(:photo) { create(:photo, user: user)}
+  let!(:user2) { create(:user) }
+  let!(:photo) { create(:photo, user: user)}
 
   describe "新規登録のテスト" do
     before do
@@ -76,6 +77,46 @@ RSpec.describe "Usersコントローラーのテスト", type: :request do
       end
       it '編集リンクが表示される' do
         expect(page).to have_link '', href: edit_user_path(user)
+      end
+    end
+  end
+
+  describe 'Users/edit' do
+    context '未ログインの場合' do
+      it 'ログインページへリダイレクトされる' do
+        visit edit_user_path(user)
+        expect(page).to have_content('ログイン')
+      end
+    end
+    context 'ログイン済の場合' do
+      before do
+        visit new_user_session_path
+        fill_in 'user[email]', with: user.email
+        fill_in 'user[password]', with: user.password
+        click_button 'ログイン'
+        visit edit_user_path(user)
+      end
+      it 'ユーザー編集画面が表示される' do
+        expect(current_path).to eq edit_user_path(user)
+      end
+      it '他ユーザー編集画面には遷移できない' do
+        visit edit_user_path(user2)
+        expect(current_path).to eq user_path(user)
+      end
+      it '画像編集フォームが表示される' do
+        expect(page).to have_field 'user[image]'
+      end
+      it '"姓"編集フォームに自分の"姓"が表示される' do
+        expect(page).to have_field 'user[last_name]', with: user.last_name
+      end
+      it '"名"編集フォームに自分の"名"が表示される' do
+        expect(page).to have_field 'user[first_name]', with: user.first_name
+      end
+      it '"セイ"編集フォームに自分の"セイ"が表示される' do
+        expect(page).to have_field 'user[read_last_name]', with: user.read_last_name
+      end
+      it '"メイ"編集フォームに自分の"メイ"が表示される' do
+        expect(page).to have_field 'user[read_first_name]', with: user.read_first_name
       end
     end
   end
